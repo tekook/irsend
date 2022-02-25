f87c8e4792ed9691f81fdb61c8d231d21209c7f4
jute <julian@tekook.de>
2022-02-25 21:49:30 +0100

    add get/setState

--- wsgi.py
+++ wsgi.py
@@ -4,21 +4,30 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 reg_name = re.compile('^[a-zA-Z0-9\-]+$')
 reg_key = re.compile('^KEY_[A-Z0-9]+$')
-
+reg_state = re.compile('^OFF$|^ON$')
+states = {'lichterkette': {'state' : 'OFF', "ON": "KEY_POWER", "OFF": "KEY_POWER2"}}
 
 
 @app.route("/")
 def welcome():
 	return render_template('welcome.html')
-@app.route("/lk/on")
-def lichton():
-	os.system("irsend SEND_ONCE lichterkette KEY_POWER")
-	return redirect(url_for('welcome'))
 
-@app.route("/lk/off")
-def lichtoff():
-	os.system("irsend SEND_ONCE lichterkette KEY_POWER2")
-	return redirect(url_for('welcome'))
+@app.route("/state/<name>")
+def getState(name):
+	if(reg_name.match(name)) and states[name]:
+		return {"state": states[name]["state"]}
+	else:
+		return "Not found", 404
+
+
+@app.route("/state/<name>/<state>")
+def setState(name:str, state: str):
+	if(reg_name.match(name) and states[name] and reg_state.match(state)):
+		subprocess.run(["irsend", "SEND_ONCE", name, states[name][state]])
+		states[name]["state"] = state
+		return {"state": states[name]["state"]}
+	else:
+		return "Not found", 404
 
 
 @app.route("/ir/<string:name>/<string:key>")
