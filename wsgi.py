65658af883ce821a313e6e9fcaae787969a827f8
jute <julian@tekook.de>
2022-02-27 13:14:56 +0100

    fix lichterkette

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ reg_name = re.compile('^[a-zA-Z0-9\-]+$')
 reg_key = re.compile('^KEY_[A-Z0-9]+$')
 reg_state = re.compile('^OFF$|^ON$')
 states = {
-	'lichterkette': {'state' : 'OFF', "ON": "KEY_POWER", "OFF": "KEY_POWER2"},
+	'lichterkette': {'state' : 'OFF', "ON": "KEY_POWER", "OFF": "KEY_POWER2", "always_off": 0},
 	'logitech-z906': {'state': 'OFF', "ON": "KEY_POWER", "OFF": "KEY_POWER", "always_off": 1},
 	'teufel': {'state': 'OFF', "ON": "KEY_POWER", "OFF": "KEY_POWER", "always_off": 1}
 	}
